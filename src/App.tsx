9fac59e15398f8c737aa66a9089e9f8f342dad55
yoj <mirrorball.pet.rolzer@gmail.com>
2020-08-21 22:44:26 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from '@aws-amplify/core';
+import awsmobile from './aws-exports';
 
-function App() {
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsmobile);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>HelloWorld</h1>
+  )
 }
 
-export default App;
+// @ts-ignore
+export default withAuthenticator(App, {
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"],
+  }
+})
